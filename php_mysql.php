fd8a21e39ce6a5fab5504a4df15d1deea1d1cbbe
zr0nis <zromirwk@gmai.com>
2022-04-17 15:36:48 +0300

    update php_mysql

--- php_mysql.php
+++ php_mysql.php
@@ -13,24 +13,22 @@
 
 	$mysql = new mysqli($var['host'], $var['login'], $var['pass'], $var['dbname']);
 
-	// $mysql->query("DROP TABLE `users_test`");
-	// $mysql->query("CREATE TABLE `users_test`(
-	// 	id INT NOT NULL AUTO_INCREMENT,
-	// 	name VARCHAR(50) NOT NULL,
-	// 	bio TEXT NOT NULL,
-	// 	PRIMARY KEY(id)
-	// )");
-	// $mysql->query("INSERT INTO
-	// 	`users_test`
-	// 		(`name`, `bio`)
-	// 		VALUES('Jone', 'FULL text')
-	// ");
-// $mysql->query("DELETE FROM
-// 		`users_test`
-// 			WHERE `id`	= 1
-// 	");
+	$mysql->query("CREATE TABLE `users_test`(
+		id INT NOT NULL AUTO_INCREMENT,
+		name VARCHAR(50) NOT NULL,
+		bio TEXT NOT NULL,
+		PRIMARY KEY(id)
+	)");
+	$mysql->query("INSERT INTO
+		`users_test`
+			(`name`, `bio`)
+			VALUES('Jone', 'FULL text')
+	");
+	$mysql->query("DELETE FROM `users_test`
+		WHERE `id`	= 1
+	");
 
-
+	$mysql->query("DROP TABLE `users_test`");
 	$mysql->close();
 ?>
 
